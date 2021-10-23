19ec23e11894cd9bb0640d30b57e67d1507c7144
suprithajanagam <suprithajanagam@yahoo.com>
2021-10-23 07:49:15 +0530

    created new webapi project

---
+++ winelegend.service/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace winelegend.service.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
