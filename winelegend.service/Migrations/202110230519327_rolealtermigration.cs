3f706b71390068238775f658208a6da2d64931b3
suprithajanagam <suprithajanagam@yahoo.com>
2021-10-23 11:00:56 +0530

    entityfreamework migrations swagger sintalation

---
+++ winelegend.service/Migrations/202110230519327_rolealtermigration.cs
@@ -0,0 +1,20 @@
+﻿namespace winelegend.service.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class rolealtermigration : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Roles", "RoleName", c => c.String(nullable: false));
+            DropColumn("dbo.Roles", "Name");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Roles", "Name", c => c.String(nullable: false));
+            DropColumn("dbo.Roles", "RoleName");
+        }
+    }
+}
