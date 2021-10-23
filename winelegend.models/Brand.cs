fbfc84f931f0159eea319ee7764e1badabebf2e5
suprithajanagam <suprithajanagam@yahoo.com>
2021-10-23 17:55:50 +0530

    model creation

---
+++ winelegend.models/Brand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace winelegend.models
+{
+    public class Brand
+    {
+        [Key]
+        public Guid BrandId { get; set; }
+        [ForeignKey("SubCategory")]
+        public Guid SubCategoryId { get; set; }
+        public string BrandName { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public string ModifiedBy { get; set; }
+
+    }
+}
