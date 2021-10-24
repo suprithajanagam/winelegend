8b37e57c2e2d1b26dace14819b70b6a921ead431
suprithajanagam <suprithajanagam@yahoo.com>
2021-10-24 09:40:04 +0530

    user model migration

---
+++ winelegend.models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace winelegend.models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ModifiedBy{get;set;}
+        public DateTime ModifiedDate { get; set; }
+        public int EmergencyContactNumber { get; set; }
+        public string EmergencyContactPersonName { get; set; }
+
+
+
+
+    }
+}
